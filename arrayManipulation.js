ac0aaf63e2845d26732759790f11b1f51d071dde
letsa11352899 <letsaeric7321@gmail.com>
2024-05-18 19:26:04 +0000

    TASK 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,10 @@
 function processArray(arr) {
     return arr.map(num => num % 2 === 0? num ** 2 : num * 3);
-  }
+   
+    function formatArrayStrings(strings, processedNumbers) {
+        return strings.map((string, index) => {
+          const processedNumber = processedNumbers[index];
+          return `${string} ${processedNumber}`;
+        });
+      }
+    }
