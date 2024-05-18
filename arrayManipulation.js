15fcc95b5385dc96a62beb0d342328ba671d8935
letsa_eric <151977555+letsa11352899@users.noreply.github.com>
2024-05-18 19:52:49 +0000

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,12 +1,1 @@
-function processArray(arr) {
-    return arr.map(num => num % 2 === 0? num ** 2 : num * 3);
-   
-    function formatArrayStrings(strings, processedNumbers) {
-        return strings.map((string, index) => {
-          const processedNumber = processedNumbers[index];
-          return processedNumber % 2 === 0
-            ? string.toUpperCase()
-            : string.toLowerCase();
-        });
-      }
-}
+
